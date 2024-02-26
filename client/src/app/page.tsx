2d5f3f57fa22b04be0fb09feae9f2acdedb96d8e
Juliette <juliette.legault@polymtl.ca>
2024-02-26 00:37:07 -0500

    setup server communication

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch("http://localhost:5124/");
+  const data = await response.text();
+  console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
