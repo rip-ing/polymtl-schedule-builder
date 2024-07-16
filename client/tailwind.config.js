c5c2c9c757a969aaa85b40153f53f24ae30c1953
Juliette <juliette.legault@polymtl.ca>
2024-07-16 15:36:08 -0400

    project config

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+import '@nextui-org/react/dist/nextui.css'
+import { nextui } from '@nextui-org/theme'
+
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [nextui()],
+}
